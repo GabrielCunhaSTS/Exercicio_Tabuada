23c03612e6dd976d4c27ca04f0a65c088f265d38
Gabriel <ggabrielcunha06@gmail.com>
2025-05-13 17:43:41 -0300

    correção

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-﻿int numero; 
+﻿int numero;
+
 Console.Write("Digite um número para ver sua tabuada: "); 
 
 while (!int.TryParse(Console.ReadLine(), out numero)) {
     Console.Write("Resposta inválida. Tente novamente digitando um número inteiro: "); 
 } 
-Console.WriteLine($"\nTabuada do número: {numero}\n"); 
+Console.WriteLine($"\nTabuada do número: {numero}"); 
 for (int x = 0; x <= 10; x++){ 
     Console.WriteLine($"{numero} x {x} = {numero * x}"); 
 }
